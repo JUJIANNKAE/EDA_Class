73d0531cabaafd8bb10613553c9cbcaff354d5f7
jklion <zhu2587746105@outlook.com>
2024-07-09 14:06:55 +0800

    正弦波仿真文件

---
+++ lab2/singen/src/sim/sml_sin.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ns
+module sml_sin ();
+
+    parameter DELAY = 200;
+    reg clk_50M;
+    reg rst;
+    wire [7:0] dataout;
+
+    singen U0 (
+        .clk    (clk_50M),
+        .dataout(dataout)
+    );
+
+    initial begin
+        rst = 0;
+        #DELAY;
+        rst = 1;
+    end
+
+    initial begin
+        clk_50M = 0;
+        forever begin
+            #10 clk_50M = !clk_50M;
+        end
+    end
+
+
+endmodule
