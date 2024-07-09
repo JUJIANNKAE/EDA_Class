fcbc49c1675bdf3b33be915987fa26b081e418b9
jklion <zhu2587746105@outlook.com>
2024-07-09 14:38:37 +0800

    绑定引脚

---
+++ lab2/singen/src/tcl/pins.tcl
@@ -0,0 +1,11 @@
+set_location_assignment PIN_AD15 -to clk
+set_location_assignment PIN_T29 -to rst
+
+set_location_assignment PIN_W27 -to dataout[0]
+set_location_assignment PIN_W25 -to dataout[1]
+set_location_assignment PIN_W23 -to dataout[2]
+set_location_assignment PIN_Y27 -to dataout[3]
+set_location_assignment PIN_Y24 -to dataout[4]
+set_location_assignment PIN_Y23 -to dataout[5]
+set_location_assignment PIN_AA27 -to dataout[6]
+set_location_assignment PIN_AA24 -to dataout[7]
