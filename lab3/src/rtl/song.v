40b1e5c1514afa1f31da5f32770d1579178f9b44
jklion <zhu2587746105@outlook.com>
2024-07-09 16:39:01 +0800

    添加梁祝

--- lab3/src/rtl/song.v
+++ lab3/src/rtl/song.v
@@ -79,6 +79,7 @@ module song (
         endcase
     end
 
+    // 两只老虎
     always @(posedge clk_4hz) begin
         if (counter == 71) counter <= 0;  //计时，实现循环演奏
         else counter <= counter + 1;
@@ -169,6 +170,151 @@ module song (
             default: {hign, med, low} <= 'h000;
         endcase
     end
+
+    // // 梁祝
+    // always @(posedge clk_4hz) begin
+    //     if (counter == 135) counter <= 0;  //计时，实现循环演奏
+    //     else counter <= counter + 1;
+    //     case (counter)
+    //         0: {hign, med, low} <= 'h003;  //低音 3，持续四个节拍
+    //         1: {hign, med, low} <= 'h003;
+    //         2: {hign, med, low} <= 'h003;
+    //         3: {hign, med, low} <= 'h003;
+    //         4: {hign, med, low} <= 'h005;  //低音 5，持续三个节拍
+    //         5: {hign, med, low} <= 'h005;
+    //         6: {hign, med, low} <= 'h005;
+    //         7: {hign, med, low} <= 'h006;
+    //         8: {hign, med, low} <= 'h010;  //中音 1，持续三个节拍
+    //         9: {hign, med, low} <= 'h010;
+    //         10: {hign, med, low} <= 'h010;
+    //         11: {hign, med, low} <= 'h020;
+    //         12: {hign, med, low} <= 'h006;
+    //         13: {hign, med, low} <= 'h010;
+    //         14: {hign, med, low} <= 'h005;
+    //         15: {hign, med, low} <= 'h005;
+    //         16: {hign, med, low} <= 'h050;
+    //         17: {hign, med, low} <= 'h050;
+    //         18: {hign, med, low} <= 'h050;
+    //         19: {hign, med, low} <= 'h100;
+    //         20: {hign, med, low} <= 'h060;
+    //         21: {hign, med, low} <= 'h050;
+    //         22: {hign, med, low} <= 'h030;
+    //         23: {hign, med, low} <= 'h050;
+    //         24: {hign, med, low} <= 'h020;
+    //         25: {hign, med, low} <= 'h020;
+    //         26: {hign, med, low} <= 'h020;
+    //         27: {hign, med, low} <= 'h020;
+    //         28: {hign, med, low} <= 'h020;
+    //         29: {hign, med, low} <= 'h020;
+    //         30: {hign, med, low} <= 'h020;
+    //         31: {hign, med, low} <= 'h020;
+    //         32: {hign, med, low} <= 'h020;
+    //         33: {hign, med, low} <= 'h020;
+    //         34: {hign, med, low} <= 'h020;
+    //         35: {hign, med, low} <= 'h030;
+    //         36: {hign, med, low} <= 'h007;
+    //         37: {hign, med, low} <= 'h007;
+    //         38: {hign, med, low} <= 'h006;
+    //         39: {hign, med, low} <= 'h006;
+    //         40: {hign, med, low} <= 'h005;
+    //         41: {hign, med, low} <= 'h005;
+    //         42: {hign, med, low} <= 'h005;
+    //         43: {hign, med, low} <= 'h006;
+    //         44: {hign, med, low} <= 'h010;
+    //         45: {hign, med, low} <= 'h010;
+    //         46: {hign, med, low} <= 'h020;
+    //         47: {hign, med, low} <= 'h020;
+    //         48: {hign, med, low} <= 'h003;
+    //         49: {hign, med, low} <= 'h003;
+    //         50: {hign, med, low} <= 'h010;
+    //         51: {hign, med, low} <= 'h010;
+    //         52: {hign, med, low} <= 'h006;
+    //         53: {hign, med, low} <= 'h005;
+    //         54: {hign, med, low} <= 'h006;
+    //         55: {hign, med, low} <= 'h001;
+    //         56: {hign, med, low} <= 'h005;
+    //         57: {hign, med, low} <= 'h005;
+    //         58: {hign, med, low} <= 'h005;
+    //         59: {hign, med, low} <= 'h005;
+    //         60: {hign, med, low} <= 'h005;
+    //         61: {hign, med, low} <= 'h005;
+    //         62: {hign, med, low} <= 'h005;
+    //         63: {hign, med, low} <= 'h005;
+    //         64: {hign, med, low} <= 'h030;
+    //         65: {hign, med, low} <= 'h030;
+    //         66: {hign, med, low} <= 'h030;
+    //         67: {hign, med, low} <= 'h050;
+    //         68: {hign, med, low} <= 'h007;
+    //         69: {hign, med, low} <= 'h007;
+    //         70: {hign, med, low} <= 'h020;
+    //         71: {hign, med, low} <= 'h020;
+    //         72: {hign, med, low} <= 'h006;
+    //         73: {hign, med, low} <= 'h010;
+    //         74: {hign, med, low} <= 'h005;
+    //         75: {hign, med, low} <= 'h005;
+    //         76: {hign, med, low} <= 'h005;
+    //         77: {hign, med, low} <= 'h005;
+    //         78: {hign, med, low} <= 'h000;
+    //         79: {hign, med, low} <= 'h000;
+    //         80: {hign, med, low} <= 'h003;
+    //         81: {hign, med, low} <= 'h005;
+    //         82: {hign, med, low} <= 'h005;
+    //         83: {hign, med, low} <= 'h003;
+    //         84: {hign, med, low} <= 'h005;
+    //         85: {hign, med, low} <= 'h006;
+    //         86: {hign, med, low} <= 'h007;
+    //         87: {hign, med, low} <= 'h020;
+    //         88: {hign, med, low} <= 'h006;
+    //         89: {hign, med, low} <= 'h006;
+    //         90: {hign, med, low} <= 'h006;
+    //         91: {hign, med, low} <= 'h006;
+    //         92: {hign, med, low} <= 'h006;
+    //         93: {hign, med, low} <= 'h006;
+    //         94: {hign, med, low} <= 'h005;
+    //         95: {hign, med, low} <= 'h006;
+    //         96: {hign, med, low} <= 'h010;
+    //         97: {hign, med, low} <= 'h010;
+    //         98: {hign, med, low} <= 'h010;
+    //         99: {hign, med, low} <= 'h020;
+    //         100: {hign, med, low} <= 'h050;
+    //         101: {hign, med, low} <= 'h050;
+    //         102: {hign, med, low} <= 'h030;
+    //         103: {hign, med, low} <= 'h030;
+    //         104: {hign, med, low} <= 'h020;
+    //         105: {hign, med, low} <= 'h020;
+    //         106: {hign, med, low} <= 'h030;
+    //         107: {hign, med, low} <= 'h020;
+    //         108: {hign, med, low} <= 'h010;
+    //         109: {hign, med, low} <= 'h010;
+    //         110: {hign, med, low} <= 'h006;
+    //         111: {hign, med, low} <= 'h005;
+    //         112: {hign, med, low} <= 'h003;
+    //         113: {hign, med, low} <= 'h003;
+    //         114: {hign, med, low} <= 'h003;
+    //         115: {hign, med, low} <= 'h003;
+    //         116: {hign, med, low} <= 'h010;
+    //         117: {hign, med, low} <= 'h010;
+    //         118: {hign, med, low} <= 'h010;
+    //         119: {hign, med, low} <= 'h010;
+    //         120: {hign, med, low} <= 'h006;
+    //         121: {hign, med, low} <= 'h010;
+    //         122: {hign, med, low} <= 'h006;
+    //         123: {hign, med, low} <= 'h005;
+    //         124: {hign, med, low} <= 'h003;
+    //         125: {hign, med, low} <= 'h005;
+    //         126: {hign, med, low} <= 'h006;
+    //         127: {hign, med, low} <= 'h001;
+    //         128: {hign, med, low} <= 'h005;  //演奏到此小节结束
+    //         129: {hign, med, low} <= 'h005;
+    //         130: {hign, med, low} <= 'h005;
+    //         131: {hign, med, low} <= 'h005;
+    //         132: {hign, med, low} <= 'h005;
+    //         133: {hign, med, low} <= 'h005;
+    //         134: {hign, med, low} <= 'h003;
+    //         135: {hign, med, low} <= 'h005;
+    //         default: {hign, med, low} <= 'h000;
+    //     endcase
+    // end
 
     led7s u1 (
         hign,
